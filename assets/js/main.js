2e70ed5a23737d50707d6a2c2e54751fd8945c40
permanayogi <yogipermana597@gmail.com>
2022-12-11 15:21:13 +0700

    finish add skills tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+			tabsContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+			const target = document.querySelector(tab.dataset.target)
 
+			tabsContents.forEach(tc => {
+				tc.classList.remove('filters__active')
+			})
+			target.classList.add('filters__active')
+
+			tabs.forEach(t => {
+				t.classList.remove('filter-tab-active')
+			})
+			tab.classList.add('filter-tab-active')
+	})
+})
 /*=============== DARK LIGHT THEME ===============*/
 
 
